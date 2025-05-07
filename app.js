b5e639640f3ce3e229e1dcc2621f634e04eb85ea
XY-THESOLVER <amexyriel102011@gmail.com>
2025-05-07 23:30:01 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// JavaScript for Observational Science
+
+console.log("Welcome to Observational Science - The Art of Deduction");
+
+// Example: Alert when the page loads
+window.onload = function() {
+    alert("Observation is a weapon. Logic is the blade.");
+};
+
+// Example: Function to change background color
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
